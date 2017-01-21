2ca06234ff26b1263626709d80ef08568359dfee
fichette <af_belkham@esi.dz>
2017-01-21 16:33:31 +0100

    Classifiers

---
+++ code/Classifiers.py
@@ -0,0 +1,36 @@
+
+def GradientBoosting_Classifier(self,max_depth,min_samples_leaf, n_estimators,learning_rate):
+    from sklearn.ensemble import GradientBoostingClassifier
+    cls =GradientBoostingClassifier(max_depth=max_depth, min_samples_leaf=min_samples_leaf,n_estimators=n_estimators,learning_rate=learning_rate,random_state=42)
+    return cls
+
+def RandomForest_Classifier(self,n_estimators):
+    from sklearn.ensemble import RandomForestClassifier
+    cls = RandomForestClassifier(n_estimators = n_estimators)
+    return cls
+
+def SVC_Classifier(self):
+    from sklearn.svm import SVC
+    cls =  SVC(kernel='rbf', C=1.0, gamma='auto')
+    return cls
+
+def MLP_Classifier(self):
+    from sklearn.neural_network import MLPClassifier
+    cls =MLPClassifier(hidden_layer_sizes=(100, ), activation='relu', batch_size='auto', learning_rate='constant', learning_rate_init=0.001, max_iter=200)
+    return cls
+
+def fit(self,cls, X, y):
+    self.cls.fit(X, y)
+
+def predict(self,cls, X):
+    return self.cls.predict(X)
+
+def score(self,cls, X, Y):
+    return self.cls.score(X,Y)
+
+def SVM_GridSearch(clf, C_range, gamma_range, crossVal, data, labels):
+    from sklearn.model_selection import GridSearchCV
+    param_grid = dict(gamma=gamma_range, C=C_range)
+    grid = GridSearchCV(clf, param_grid=param_grid, cv=crossVal)
+    grid.fit(data, labels)
+    return grid.best_params_, grid.best_score_
